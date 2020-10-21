2045978007e47cca515dbf0010d94834c44e7979
Leonid Podriz <leonidpodriz@gmail.com>
2020-10-21 21:06:30 +0300

    :memo: Add core models and migrations
    Add UnregisteredUser, ContactMessage, Social models and migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class UnregisteredUser(models.Model):
+    name = models.CharField(max_length=20)
+    web_site = models.URLField(max_length=255)
+    email = models.EmailField()
+    ip = models.GenericIPAddressField(blank=True, null=True)
+
+
+class ContactMessage(models.Model):
+    message = models.TextField()
+    author = models.ForeignKey(UnregisteredUser, on_delete=models.CASCADE)
+
+
+class Social(models.Model):
+    name = models.CharField(max_length=20)
+    icon_name = models.CharField(max_length=20)
+    link = models.URLField(max_length=255)
