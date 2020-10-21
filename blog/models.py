1688245e1818fb0dd77a1bbe4ecd766ba2291119
Leonid Podriz <leonidpodriz@gmail.com>
2020-10-21 21:07:30 +0300

    :memo: Add blog models and migrations
    Add Category, Tag, Post, Comments models and migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from core.models import UnregisteredUser
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=10)
+    slug = models.SlugField(max_length=10)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+    title = models.CharField(max_length=20)
+    categories = models.ManyToManyField(Category)
+    datetime = models.DateTimeField(auto_now_add=True)
+    datetime_edit = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag)
+    slug = models.SlugField(max_length=20)
+
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    message = models.TextField()
+    commentator = models.ForeignKey(UnregisteredUser, on_delete=models.CASCADE)
